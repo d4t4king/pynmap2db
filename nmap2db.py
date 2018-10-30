f73f8626e3dde011965595b8a578a3eeeda354f9
dataking <dataking@gmail.com>
2018-10-29 17:01:19 -0700

    WIP: adding hosts.  Still need to populate other tables.

--- nmap2db.py
+++ nmap2db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import csv
 import time
 import pprint
@@ -144,12 +145,61 @@ def create_database(_dbfile):
 	dbfile = Path(_dbfile)
 	if dbfile.exists():
 		if dbfile.is_dir():
-			raise("{0} is a directory.  I don't know how to handle those (yet).")
-		elif dbfile.is_file()():
-			raise("File already exists.  Sheepishly refusing to overwrite.")
+			raise RuntimeError("{0} is a directory.  I don't know how to handle those (yet).")
+		# if the file exists, assume that we're appending an existing DB
+		#elif dbfile.is_file():
+		#	raise RuntimeError("File already exists.  Sheepishly refusing to overwrite.")
 	else:
 		for t in [ 'nmap', 'hosts', 'sequencing', 'ports', 'os' ]:
 			create_table(t, _dbfile)
+
+def check_scan_record(dbfile, scanargs, starttime, endtime):
+	conn = sqlite3.connect(dbfile)
+	c = conn.cursor()
+	c.execute("SELECT sid FROM nmap WHERE args='{0}' AND starttime='{1}' AND endtime='{2}'".format(scanargs, starttime, endtime))
+	r = c.fetchone()
+	if r is not None:
+		return r[0]
+	else:
+		return None
+
+def do_sql(_dbfile, sql):
+	pp = pprint.PrettyPrinter(indent=4)
+	conn = sqlite3.connect(_dbfile)
+	c = conn.cursor()
+	c.execute(sql)
+	match = re.search(r'(insert|update)', sql)
+	if match:
+		return None
+	else:
+		# assume it's a select statement
+		r = c.fetchall()
+	conn.commit()
+	conn.close()
+
+	if r is not None and len(r) > 0:
+		try:
+			return r[0]
+		except IndexError as idxerr:
+			print("DEBUG: pprint(r):")
+			pp.pprint(r)
+			raise(idxerr)
+	else:
+		return None
+
+def get_tcpcount(hostobj):
+	count = -1
+	for tup in hostobj.get_ports():
+		if 'tcp' in tup[1]:
+			count += 1
+	return count
+
+def get_udpcount(hostobj):
+	count = -1
+	for tup in hostobj.get_ports():
+		if 'udp' in tup[1]:
+			count += 1
+	return count
 
 def main():
 	args = handle_args()
@@ -180,6 +230,59 @@ def main():
 					# if verbose:
 					#print(row)
 					writer.writerow(row)
+		elif args.format=='sqlite':
+			create_database(args.dbfile)
+			#print(dir(nmap))
+			sid = check_scan_record(args.dbfile, nmap.commandline, \
+				nmap.started, nmap.endtime)
+			if sid is not None and \
+				'int' in str(type(sid)):
+				print("Scan record already exists. SID='{0}'".format(sid))
+			else:
+				#pp.pprint(nmap.get_raw_data())
+				sql1 = "INSERT INTO nmap (version, xmlversion, args, types, \
+	starttime, startstr, endtime, endstr, numservices) VALUES ('%s', '%s', \
+	'%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (nmap.version, \
+	nmap.get_raw_data()['_nmaprun']['xmloutputversion'], nmap.commandline, \
+	nmap.scan_type, nmap.started, nmap.get_raw_data()['_nmaprun']['startstr'], \
+	nmap.endtime, nmap.endtimestr, nmap.get_raw_data()['_scaninfo']['numservices'])
+				do_sql(args.dbfile, sql1)
+				sid = check_scan_record(args.dbfile, nmap.commandline, \
+					nmap.started, nmap.endtime)
+			#print("DEBUG: SID is {0}".format(sid))
+			if len(nmap.hosts) == 0 or nmap.hosts is None:
+				print("There are no hosts in this scan.")
+			else:
+				for h in nmap.hosts:
+					# check if the host is already in the database?
+					hid = do_sql(args.dbfile, "SELECT hid FROM hosts WHERE \
+	ip4='{0}' and sid='{1}'".format(h.address, sid))
+					print("DEBUG: HID[1]={0}".format(hid))
+					if 'int' in str(type(hid)) and hid > 0:
+						print("Host record exists with hid='{0}'".format(hid))
+						continue
+					if h.hostnames is not None:
+						if 'list' in str(type(h.hostnames)):
+							if len(h.hostnames) > 0:
+								pp.pprint(h.hostnames)
+								hostname = h.hostnames[0]
+							else:
+								hostname = ''
+						else:
+							hostname = h.hostnames
+					else:
+						hostname = ''
+					sql2 = "INSERT INTO hosts (sid, ip4, ip4num, hostname, \
+	status, tcpcount, udpcount, mac, vendor, ip6, distance, uptime, upstr) \
+	VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
+	'%s', '%s')" % (sid, h.ipv4, '[ip4num]', hostname, h.status, \
+	get_tcpcount(h), get_udpcount(h), h.mac, h.vendor, h.ipv6, h.distance, \
+	h.uptime, 'upstr')
+					do_sql(args.dbfile, sql2)
+					print("Host record inserted.")
+					hid = do_sql(args.dbfile, "SELECT hid FROM hosts WHERE \
+	ip4='{0}' and sid='{1}'".format(h.address, sid))
+					print("DEBUG: HID[2]={0}".format(hid))
 		else:
 			raise Exception("Unknown format: {0}".format(args.format))
 		
